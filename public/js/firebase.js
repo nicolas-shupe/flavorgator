517a9262b9b4552dc881fa519193e421b35546cd
ftc359 <ftc.359@gmail.com>
2017-03-17 19:56:12 -0700

    Initial commit

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+// <script src="https://www.gstatic.com/firebasejs/3.7.2/firebase.js"></script>
+document.addEventListener('DOMContentLoaded', function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAasOds-5xi2RPUiy4tbB5hiLfdjlAdIqw",
+    authDomain: "flavor-gator-95ca4.firebaseapp.com",
+    databaseURL: "https://flavor-gator-95ca4.firebaseio.com",
+    storageBucket: "flavor-gator-95ca4.appspot.com",
+    messagingSenderId: "477854566477"
+  };
+  firebase.initializeApp(config);
+}());
