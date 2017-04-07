23b697d6ac23b3febfdaa0ef654c401c5397f222
nicolas-shupe <kve@KVEs-MacBook-Pro.local>
2017-04-08 07:40:00 +0900

    Stuffs and stuffs and stuffs

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
-var functions = require('firebase-functions');
+/*'use strict;'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+const functions = require('firebase-functions');
+
+const moment = require('moment');
+
+const cors = require('cors') ({origin: true});
+
+exports.date = functions.https.onRequest((req, res => {
+    if (req.method == 'PUT') {
+        res.statusCode('403').send();
+    }
+
+    cors.onRequest(req, res, () => {
+        let format = req.query.format;
+        if (!format) {
+            format = req.body.format;
+        }
+        const formattedDate = moment().format(format);
+        console.log('Sending Formatted date:', formattedDate);
+        res.status(200).send(formattedDate);
+    });
+}));*/
